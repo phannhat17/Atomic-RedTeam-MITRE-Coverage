04a41594fa584829944349f7950ed0172d9012bc
Ha Chu Minh <Ha.CM210293@sis.hust.edu.vn>
2023-06-06 17:11:00 +0700

    Update URL usage to URI

--- demo/src/main/java/com/example/GetMITREData.java
+++ demo/src/main/java/com/example/GetMITREData.java
@@ -1,39 +1,41 @@
 package com.example;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
+import java.io.*;
+import java.net.*;
+import java.nio.channels.*;
 
-public class GetMITREData {
+public class GetMITREData 
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException 
+    {
         String downloadPath = "data/mitre-attack";
         File directory = new File(downloadPath);
-        if (!directory.exists()) {
+        if (!directory.exists()) 
+        {
             directory.mkdirs();
         }
         String downloadUrl = "https://raw.githubusercontent.com/mitre-attack/attack-stix-data/master/";
-        String[] filesPath = { 
-                "enterprise-attack/enterprise-attack.json", 
-                "mobile-attack/mobile-attack.json",
-                "ics-attack/ics-attack.json" 
-            };
-        for (String path : filesPath) {
-            try {
+        String[] filesPath = {"enterprise-attack/enterprise-attack.json", "mobile-attack/mobile-attack.json",
+                	"ics-attack/ics-attack.json"};
+        for (String path : filesPath) 
+        {
+            try 
+            {
                 String fileName = path.substring(path.lastIndexOf('/') + 1);
                 downloadUsingNIO(downloadUrl + path, downloadPath + "/" + fileName);
-            } catch (IOException e) {
+            } 
+            catch (IOException e) 
+            {
                 e.printStackTrace();
             }
         }
     }
 
-    private static void downloadUsingNIO(String urlStr, String file) throws IOException {
+    private static void downloadUsingNIO(String urlStr, String file) throws IOException, URISyntaxException 
+    {
         long start = System.currentTimeMillis();
-        URL url = new URL(urlStr);
+        URL url = new URI(urlStr).toURL();
         ReadableByteChannel rbc = Channels.newChannel(url.openStream());
         FileOutputStream fos = new FileOutputStream(file);
         fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
