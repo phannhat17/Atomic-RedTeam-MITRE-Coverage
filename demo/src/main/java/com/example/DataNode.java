12decc5ad7ea09a14fe3ad1c171522447efc47c8
Ha Chu Minh <Ha.CM210293@sis.hust.edu.vn>
2023-06-11 14:58:57 +0700

    Create new classes for data storing - data preprocessing of coverage calculation

---
+++ demo/src/main/java/com/example/DataNode.java
@@ -0,0 +1,51 @@
+package com.example;
+
+import java.util.*;
+
+public class DataNode
+{
+	private String name;
+	private int value;
+	private Map<String, DataNode> child;
+
+	public DataNode(String name)
+	{
+		this.name = name;
+		this.value = 0;
+		this.child = new HashMap<>();
+	}
+
+	public DataNode(String name, int value)
+	{
+		this.name = name;
+		this.value = value;
+		this.child = new HashMap<>();
+	}
+
+	public DataNode(String name, int value, Map<String, DataNode> child)
+	{
+		this.name = name;
+		this.value = value;
+		this.child = child;
+	}
+
+	public int getValue()
+	{
+		return value;
+	}
+
+	public void setValue(int value)
+	{
+		this.value = value;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public Map<String, DataNode> getChild()
+	{
+		return child;
+	}
+}
