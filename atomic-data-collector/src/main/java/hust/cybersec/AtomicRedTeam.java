9b9832827d2aaa8307abb7ddf93df137aebed6a2
Phan Nhat <phandinhnhat1708@gmail.com>
2023-06-13 12:05:39 +0700

    move file to new project destination

---
+++ atomic-data-collector/src/main/java/hust/cybersec/AtomicRedTeam.java
@@ -0,0 +1,169 @@
+package com.example;
+
+import java.io.*;
+import java.net.URISyntaxException;
+
+import com.fasterxml.jackson.annotation.*;
+
+public class AtomicRedTeam extends MitreAttackFramework
+{
+	private int testNumber;
+
+	@JsonProperty("name")
+	private String testName;
+
+	@JsonProperty("auto_generated_guid")
+	private String testGuid;
+
+	@JsonProperty("description")
+	private String testDescription;
+
+	@JsonProperty("supported_platforms")
+	private String[] testSupportedPlatforms;
+
+	@JsonProperty("input_arguments")
+	private String[] testInputArguments;
+
+	@JsonProperty("executor")
+	private String[] testExecutor;
+
+	@JsonProperty("dependency_executor_name")
+	private String testDependencyExecutorName;
+
+	@JsonProperty("dependencies")
+	private String[] testDependencies;
+
+	public AtomicRedTeam()
+	{
+		super();
+	}
+
+	public AtomicRedTeam(String testName, String testGuid, String testDescription, String[] testSupportedPlatforms,
+			String[] testInputArguments, String[] testExecutor, String testDependencyExecutorName,
+			String[] testDependencies)
+	{
+		super();
+		this.testName = testName;
+		this.testGuid = testGuid;
+		this.testDescription = testDescription;
+		this.testSupportedPlatforms = testSupportedPlatforms;
+		this.testInputArguments = testInputArguments;
+		this.testExecutor = testExecutor;
+		this.testDependencyExecutorName = testDependencyExecutorName;
+		this.testDependencies = testDependencies;
+	}
+
+	public AtomicRedTeam(int testNumber, String testName, String testGuid, String testDescription,
+			String[] testSupportedPlatforms, String[] testInputArguments, String[] testExecutor,
+			String testDependencyExecutorName, String[] testDependencies)
+	{
+		super();
+		this.testNumber = testNumber;
+		this.testName = testName;
+		this.testGuid = testGuid;
+		this.testDescription = testDescription;
+		this.testSupportedPlatforms = testSupportedPlatforms;
+		this.testInputArguments = testInputArguments;
+		this.testExecutor = testExecutor;
+		this.testDependencyExecutorName = testDependencyExecutorName;
+		this.testDependencies = testDependencies;
+	}
+
+	public void setTestNumber(int testNumber)
+	{
+		this.testNumber = testNumber;
+	}
+
+	public int getTestNumber()
+	{
+		return testNumber;
+	}
+
+	public String getTestName()
+	{
+		return testName;
+	}
+
+	public String getTestGuid()
+	{
+		return testGuid;
+	}
+
+	public String getTestDescription()
+	{
+		return testDescription;
+	}
+
+	public String[] getTestSupportedPlatforms()
+	{
+		return testSupportedPlatforms;
+	}
+
+	public String[] getTestInputArguments()
+	{
+		return testInputArguments;
+	}
+
+	public String[] getTestExecutor()
+	{
+		return testExecutor;
+	}
+
+	public String getTestDependencyExecutorName()
+	{
+		return testDependencyExecutorName;
+	}
+
+	public String[] getTestDependencies()
+	{
+		return testDependencies;
+	}
+
+	public void downloadData() throws URISyntaxException
+	{
+		String atomicURL = "https://raw.githubusercontent.com/redcanaryco/atomic-red-team/master/atomics/Indexes/";
+		String atomicDirectory = "demo/data/atomic";
+		String[] atomicFiles = { "index.yaml" };
+
+		DataRetriever atomicDownloader = new DataRetriever(atomicURL, atomicDirectory, atomicFiles);
+		atomicDownloader.download("atomic-all.yaml");
+
+		String yamlFilePath = "demo/data/atomic/atomic-all.yaml";
+		String jsonFilePath = "demo/data/atomic/atomic-all.json";
+
+		YamlToJsonConverter converter = new YamlToJsonConverter(yamlFilePath, jsonFilePath);
+		converter.convert();
+	}
+
+	public void exportExcel() throws FileNotFoundException, IOException
+	{
+		String jsonFilePath = "demo/data/atomic/atomic-all.json";
+		String excelFilePath = "demo/data/atomic/atomic-all.xlsx";
+		ExcelExporter exporter = new ExcelExporter(jsonFilePath, excelFilePath);
+		exporter.export();
+	}
+
+	public int getNumTechniques(String taxonomy, String type)
+	{
+		return 0;
+	}
+
+	public void analyseCoverage(MitreAttackFramework mitre)
+	{
+		CoverageAnalyser analyser = new CoverageAnalyser(this, mitre);
+		analyser.generateChart();
+	}
+
+	public static void main(String[] args) throws URISyntaxException
+	{
+		AtomicRedTeam atomic = new AtomicRedTeam();
+		// atomic.downloadData();
+		try
+		{
+			atomic.exportExcel();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
---
+++ atomic-data-collector/src/main/java/hust/cybersec/MitreAttackFramework.java
@@ -0,0 +1,168 @@
+package com.example;
+
+import java.net.URISyntaxException;
+
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(using = Deserializer.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MitreAttackFramework
+{
+	private String techniqueId;
+
+	@JsonProperty("name")
+	private String techniqueName;
+
+	@JsonProperty("description")
+	private String techniqueDescription;
+
+	@JsonProperty("x_mitre_platforms")
+	private String[] techniquePlatforms;
+
+	@JsonProperty("x_mitre_domains")
+	private String[] techniqueDomains;
+
+	private String techniqueUrl;
+
+	private String[] techniqueTactics;
+
+	@JsonProperty("x_mitre_detection")
+	private String techniqueDetection;
+
+	@JsonProperty("x_mitre_is_subtechnique")
+	private boolean techniqueIsSubtechnique;
+
+	public MitreAttackFramework()
+	{
+
+	}
+
+	public MitreAttackFramework(String techniqueId, String techniqueName, String techniqueDescription,
+			String[] techniquePlatforms, String[] techniqueDomains, String techniqueUrl, String[] techniqueTactics,
+			String techniqueDetection, boolean techniqueIsSubtechnique)
+	{
+		this.techniqueId = techniqueId;
+		this.techniqueName = techniqueName;
+		this.techniqueDescription = techniqueDescription;
+		this.techniquePlatforms = techniquePlatforms;
+		this.techniqueDomains = techniqueDomains;
+		this.techniqueUrl = techniqueUrl;
+		this.techniqueTactics = techniqueTactics;
+		this.techniqueDetection = techniqueDetection;
+		this.techniqueIsSubtechnique = techniqueIsSubtechnique;
+	}
+
+	public String getTechniqueId()
+	{
+		return techniqueId;
+	}
+
+	public String getTechniqueName()
+	{
+		return techniqueName;
+	}
+
+	public void setTechniqueId(String techniqueId)
+	{
+		this.techniqueId = techniqueId;
+	}
+
+	public void setTechniqueName(String techniqueName)
+	{
+		this.techniqueName = techniqueName;
+	}
+
+	public void setTechniqueDescription(String techniqueDescription)
+	{
+		this.techniqueDescription = techniqueDescription;
+	}
+
+	public void setTechniquePlatforms(String[] techniquePlatforms)
+	{
+		this.techniquePlatforms = techniquePlatforms;
+	}
+
+	public void setTechniqueDomains(String[] techniqueDomains)
+	{
+		this.techniqueDomains = techniqueDomains;
+	}
+
+	public void setTechniqueUrl(String techniqueUrl)
+	{
+		this.techniqueUrl = techniqueUrl;
+	}
+
+	public void setTechniqueTactics(String[] techniqueTactics)
+	{
+		this.techniqueTactics = techniqueTactics;
+	}
+
+	public void setTechniqueDetection(String techniqueDetection)
+	{
+		this.techniqueDetection = techniqueDetection;
+	}
+
+	public void setTechniqueIsSubtechnique(boolean techniqueIsSubtechnique)
+	{
+		this.techniqueIsSubtechnique = techniqueIsSubtechnique;
+	}
+
+	public String getTechniqueDescription()
+	{
+		return techniqueDescription;
+	}
+
+	public String[] getTechniquePlatforms()
+	{
+		return techniquePlatforms;
+	}
+
+	public String[] getTechniqueDomains()
+	{
+		return techniqueDomains;
+	}
+
+	public String getTechniqueUrl()
+	{
+		return techniqueUrl;
+	}
+
+	public String[] getTechniqueTactics()
+	{
+		return techniqueTactics;
+	}
+
+	public String getTechniqueDetection()
+	{
+		return techniqueDetection;
+	}
+
+	public boolean isTechniqueIsSubtechnique()
+	{
+		return techniqueIsSubtechnique;
+	}
+
+	public void downloadData() throws URISyntaxException
+	{
+		String mitreURL = "https://raw.githubusercontent.com/mitre-attack/attack-stix-data/master/";
+		String mitreDirectory = "demo/data/mitre-attack";
+		// Array of files to download
+		String[] mitreFiles = { "enterprise-attack/enterprise-attack.json", "mobile-attack/mobile-attack.json",
+				"ics-attack/ics-attack.json" };
+
+		DataRetriever mitreDownloader = new DataRetriever(mitreURL, mitreDirectory, mitreFiles);
+		mitreDownloader.download();
+	}
+
+	public int getNumTechniques(String taxonomy, String type)
+	{
+		return 0;
+	}
+
+	public static void main(String[] args) throws URISyntaxException
+	{
+		MitreAttackFramework mitre = new MitreAttackFramework();
+		mitre.downloadData();
+	}
+}
