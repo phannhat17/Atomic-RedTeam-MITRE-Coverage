cf0a15dff4fdbd313ba20fa96c811eceea0e71e4
Phan Nhat <phandinhnhat1708@gmail.com>
2023-06-22 14:19:42 +0700

    refactor and add GUI

---
+++ atomic-data-collector/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module hust.cybersec.atomicdatacollector {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.dataformat.yaml;
+    requires org.yaml.snakeyaml;
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
+
+
+    opens hust.cybersec.screen to javafx.fxml;
+    opens hust.cybersec.data.process to com.fasterxml.jackson.databind;
+    exports hust.cybersec.screen;
+}
