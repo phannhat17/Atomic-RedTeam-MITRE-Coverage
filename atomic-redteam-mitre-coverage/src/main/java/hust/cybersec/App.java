aef425d293f65bac5d7243cc22ae2b4bb80d7a50
Phan Nhat <phandinhnhat1708@gmail.com>
2023-07-07 16:47:55 +0700

    Add console options

--- atomic-redteam-mitre-coverage/src/main/java/hust/cybersec/App.java
+++ atomic-redteam-mitre-coverage/src/main/java/hust/cybersec/App.java
@@ -5,15 +5,60 @@ import hust.cybersec.data.model.MitreAttackFramework;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class App {
     static MitreAttackFramework mitre = new MitreAttackFramework();
     static AtomicRedTeam atomic = new AtomicRedTeam();
 
     public static void main(String[] args) throws URISyntaxException, IOException {
-//		mitre.downloadData();
-//		atomic.downloadData();
-//		atomic.exportExcel();
-        atomic.analyseCoverage();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Atomic RedTeam - MITRE Coverage app!");
+
+        while (true) {
+            System.out.println("Choose an option:");
+            System.out.println("1. Download Data");
+            System.out.println("2. Export Excel");
+            System.out.println("3. View Analysis Chart");
+            System.out.println("0. Exit");
+
+            try {
+                int option = scanner.nextInt();
+                scanner.nextLine(); // Consume the newline character
+
+                switch (option) {
+                    case 1:
+                        mitre.downloadData();
+                        atomic.downloadData();
+                        break;
+                    case 2:
+                        atomic.exportExcel();
+                        break;
+                    case 3:
+                        atomic.analyseCoverage();
+                        break;
+                    case 0:
+                        System.out.println("Exiting...");
+                        return;
+                    default:
+                        System.out.println("Invalid option. Please try again.");
+                        continue;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a number.");
+                scanner.nextLine(); // Consume the invalid input
+                continue;
+            }
+
+            System.out.println("Do you want to perform another action? (Enter 'y' to continue or any other key to exit)");
+            String continueOption = scanner.nextLine();
+
+            if (!continueOption.equalsIgnoreCase("y")) {
+                System.out.println("Exiting...");
+                break;
+            }
+        }
     }
 }
